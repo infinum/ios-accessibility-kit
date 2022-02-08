92559e3ead62f5f643eaf6cf46941d048d6f8865
Nikola Majcen <nikola.majcen@infinum.hr>
2022-02-08 14:40:18 +0100

    Initial commit

---
+++ Sources/AccessibilityKit/AccessibilityKit.swift
@@ -0,0 +1,6 @@
+public struct AccessibilityKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AccessibilityKitTests/AccessibilityKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AccessibilityKit
+
+final class AccessibilityKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AccessibilityKit().text, "Hello, World!")
+    }
+}
