22f78eea87d2f257d7f799a6816085bfc5d6544f
Nikola Majcen <nikola.majcen@infinum.hr>
2022-02-08 17:17:00 +0100

    Added example project

---
+++ Example/AccessibilityKit/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AccessibilityKit
+//
+//  Created by Nikola Majcen on 08.02.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
