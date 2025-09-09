457bb046dd7703c99dcae6ac7c5a71043a495a96
hhrvoic <hrvoje.hrvoic@infinum.hr>
2025-09-09 13:41:30 +0200

    Adds more extensions, fixes equatable

--- Sources/AccessibilityKit/Classes/AccessibilityValue.swift
+++ Sources/AccessibilityKit/Classes/AccessibilityValue.swift
@@ -13,3 +13,52 @@ public enum AccessibilityValue {
     case scale(Double)
     case percentage(Double)
 }
+
+extension AccessibilityValue: Equatable {
+
+    public static func == (lhs: AccessibilityValue, rhs: AccessibilityValue) -> Bool {
+        switch (lhs, rhs) {
+        case let (.flag(lhsValue), .flag(rhsValue)):
+            return lhsValue == rhsValue
+        case let (.number(lhsValue), .number(rhsValue)):
+            return lhsValue == rhsValue
+        case let (.scale(lhsValue), .scale(rhsValue)):
+            return lhsValue == rhsValue
+        case let (.percentage(lhsValue), .percentage(rhsValue)):
+            return lhsValue == rhsValue
+        default:
+            return false
+        }
+    }
+}
+
+public extension AccessibilityValue {
+
+    var flagValue: Bool {
+        if case .flag(let isOn) = self {
+            return isOn
+        }
+        return false
+    }
+
+    var numberValue: Double? {
+        if case .number(let number) = self {
+            return number
+        }
+        return nil
+    }
+
+    var scaleValue: Double? {
+        if case .scale(let scale) = self {
+            return scale
+        }
+        return nil
+    }
+
+    var percentageValue: Double? {
+        if case .percentage(let percentage) = self {
+            return percentage
+        }
+        return nil
+    }
+}
